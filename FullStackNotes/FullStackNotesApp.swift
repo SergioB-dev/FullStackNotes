8d2e3e5268feb062134ead22325927261f11dc5f
Shakur <bostshakur@gmail.com>
2021-08-18 18:43:22 -0400

    Intiial commit

---
+++ FullStackNotes/FullStackNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullStackNotesApp.swift
+//  FullStackNotes
+//
+//  Created by Sergio Bost on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct FullStackNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
